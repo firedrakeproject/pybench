a328379866e7a09a94153adbf96c400895b36a49
Florian Rathgeber <florian.rathgeber@gmail.com>
2014-04-22 09:07:56 +0100

    Add test for save and load

--- test_pybench.py
+++ test_pybench.py
@@ -31,3 +31,11 @@ def test_timed_region():
     result = TimedRegion().run()
     assert result['timings']['total'] > 0.0
     assert result['timings']['stuff'] > 0.0
+
+
+def test_save_load(tmpdir):
+    b = TimedRegion()
+    d = tmpdir.join(b.name).strpath
+    result = b.run()
+    b.save(d)
+    TimedRegion().load(d) == result
