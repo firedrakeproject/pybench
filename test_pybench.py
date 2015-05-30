cca0a687314f709190a18a6372490ac14a41fa1e
Florian Rathgeber <florian.rathgeber@gmail.com>
2015-05-30 19:48:56 +0100

    Remove sleep unit test

--- test_pybench.py
+++ test_pybench.py
@@ -1,5 +1,4 @@
 from itertools import product
-from time import sleep
 
 from pybench import Benchmark
 
@@ -23,17 +22,6 @@ def test_no_params():
 
 def test_no_repeats():
     assert not Benchmark().run(method=lambda: None, repeats=0)['timings']
-
-
-def test_sleep():
-    def myfunc(n, duration):
-        for _ in range(n):
-            sleep(duration)
-    times = Benchmark().run(method=myfunc,
-                            params=[('n', range(3)),
-                                    ('duration', (0.001, 0.002))])
-    for (n, d), t in times['timings'].items():
-        assert abs(n*d - t['total']) < 1e-3
 
 
 def test_timed_region():
